4970baee8a063eed7896cd3721211cbf5490981e
Trogdan <mr.trogdan@gmail.com>
2016-02-14 20:06:27 -0500

    oh hey, instructions said add interstitial, not replace with interstitial

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
 import com.google.android.gms.ads.doubleclick.PublisherInterstitialAd;
 
@@ -29,6 +30,15 @@ public class MainActivityFragment extends Fragment {
         mParentActivity = (MainActivity)getActivity();
 
         View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
 
         mPublisherInterstitialAd = new PublisherInterstitialAd(getActivity());
         mPublisherInterstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
